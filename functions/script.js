d6724a69e9c08886160bc09902ac90be8727c457
sanjeev0303 <sanjeev.081021@gmail.com>
2025-07-09 21:52:44 +0530

    remove commits

--- functions/script.js
+++ functions/script.js
@@ -1,30 +1,30 @@
 //  Use rest parameter to accept any number of scores and reutnr the total.
 
-// function getScore(...scores) {
-//   let total = 0;
-//   scores.forEach((score) => {
-//     total = total + score;
-//   });
-//   return total;
-// }
-// let score = getScore(1, 2, 3, 4, 5, 6, 7, 8);
-// console.log("total score: ", score);
+function getScore(...scores) {
+  let total = 0;
+  scores.forEach((score) => {
+    total = total + score;
+  });
+  return total;
+}
+let score = getScore(1, 2, 3, 4, 5, 6, 7, 8);
+console.log("total score: ", score);
 
 // Pass a function into another funciton and execute it inside.
-// function abcd(val){
-// val()
-// }
-// abcd(function(){
-//     console.log("hey");
-// })
+function abcd(val){
+val()
+}
+abcd(function(){
+    console.log("hey");
+})
 
 // Write a BMI calculator
-// function bmi (weight, height){
-// return weight / (height * height)
-// }
+function bmi (weight, height){
+return weight / (height * height)
+}
 
-// const value = bmi(69, 1.7).toFixed(2)
-// console.log(value);
+const value = bmi(69, 1.7).toFixed(2)
+console.log(value);
 
 // Create a resuable discount calculator(HOF)
 function discontCalculator(discount) {
